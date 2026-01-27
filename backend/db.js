7cd47287bca94c55d7ecce8b55e263bc3ed77d70
Tenzin <tenzingendun99@gmail.com>
2026-01-26 23:54:21 -0600

    env changes

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
-const {Pool} = require('pg')
+require('dotenv').config();
+const {Pool} = require('pg');
 const pool = new Pool ({
-    user: 'postgres',
-    password: '',
-    hosts: "localhost",
-    port: 3000,
-    database: 'applicationtracker' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT 
 });
 
 async function addData(userId, companyName, dateApplied, statuses, incomingPhone, incomingInterview, notes) {
