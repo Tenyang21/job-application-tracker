958fdc69350b0b232f442dc856c416607cf8eccf
Tenzin <tenzingendun99@gmail.com>
2026-02-13 01:44:19 -0600

    db getUserByEmail function added

--- backend/db.js
+++ backend/db.js
@@ -149,10 +149,19 @@ async function signup(email, hash) {
 }
 async function getUserData() {
   try {
-    const result = await pool.query(
-      "SELECT * FROM users",
-    );
+    const result = await pool.query("SELECT * FROM users");
     return result.rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+async function getUserByEmail(email) {
+  try {
+    const result = await pool.query("SELECT * FROM users where email = $1", [
+      email,
+    ]);
+    return result.rows[0];
   } catch (error) {
     console.log(error);
     throw error;
@@ -170,4 +179,5 @@ module.exports = {
   upcomingEvents,
   signup,
   getUserData,
+  getUserByEmail,
 };
