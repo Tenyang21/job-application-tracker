de0514f0e7dc32da551b8569fbd8f98c756f33d7
Tenzin <tenzingendun99@gmail.com>
2026-02-24 00:05:00 -0600

    position added

--- backend/db.js
+++ backend/db.js
@@ -16,10 +16,11 @@ async function addData(
   incomingPhone,
   incomingInterview,
   notes,
+  position,
 ) {
   try {
     const result = await pool.query(
-      "INSERT INTO applicationtracker (user_id, company_name, date_applied, statuses, incoming_phone, incoming_interview, notes) Values ($1,$2,$3,$4,$5,$6,$7) RETURNING *",
+      "INSERT INTO applicationtracker (user_id, company_name, date_applied, statuses, incoming_phone, incoming_interview, notes, position) Values ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
       [
         userId,
         companyName,
@@ -28,6 +29,7 @@ async function addData(
         incomingPhone,
         incomingInterview,
         notes,
+        position,
       ],
     );
     // "Returning *" returns the newly created row with all columns.
@@ -54,7 +56,7 @@ async function getCountReplies(userId) {
   try {
     const result = await pool.query(
       "SELECT Count(*) From applicationtracker WHERE user_id = $1 AND (statuses = $2 OR statuses = $3 OR statuses = $4)",
-      [userId, "rejected", "ongoing", "offer"],
+      [userId, "phone_screen", "interviewing", "offer"],
     );
     return result.rows[0];
   } catch (error) {
@@ -92,10 +94,11 @@ async function updateData(
   incomingPhone,
   incomingInterview,
   notes,
+  position,
 ) {
   try {
     const result = await pool.query(
-      "Update applicationtracker SET company_name = $1, date_applied = $2, statuses = $3, incoming_phone = $4, incoming_interview = $5, notes = $6 Where user_id = $7 AND application_id = $8",
+      "Update applicationtracker SET company_name = $1, date_applied = $2, statuses = $3, incoming_phone = $4, incoming_interview = $5, notes = $6, position = $7 Where user_id = $8 AND application_id = $9",
       [
         companyName,
         dateApplied,
@@ -103,6 +106,7 @@ async function updateData(
         incomingPhone,
         incomingInterview,
         notes,
+        position,
         userId,
         id,
       ],
